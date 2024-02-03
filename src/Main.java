06de4d2067feb57628507d2bf17f246885c71cc9
ChDon08 <chdon08@gmail.com>
2024-02-03 16:44:20 +0100

    fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ void main() throws InterruptedException {
     Scanner in = new Scanner(System.in); //scanner
     int x, y; //coordinate
     String[][] campo;
-    campo = new String[3][3];
+    campo = new String[1000][1000];
     campo = griglia(campo);
     print(campo);
     String modalita;
@@ -45,22 +45,20 @@ void main() throws InterruptedException {
 
     while (true) {
         //giocatore
-        System.out.println();
-        System.out.println("inserie coordinata y");
-        y = in.nextInt();
-        System.out.println("inserie coordinata x");
-        x = in.nextInt();
-        if (x > 2 || y > 2) {
-            System.out.println("sei un mona, rifai!");
-        }
-        if (campo[y][x].equals("*")) {
-            System.out.println("prefetto");
-        } else {
-            System.out.println("casella già occupata, sei un mona");
-        }
-        if ((x <= 2 && y <= 2) && campo[y][x].equals("*")) {
-            campo[y][x] = "x";
-        }
+        do {
+            System.out.println();
+            System.out.println("inserie coordinata y");
+            y = in.nextInt();
+            System.out.println("inserie coordinata x");
+            x = in.nextInt();
+            if ((x > 2 || y > 2) || !campo[y][x].equals("*")) {
+                System.out.println("sei un mona, rifai!");
+            }
+            if (campo[y][x].equals("*")) {
+                System.out.println("prefetto");
+                campo[y][x] = "x";
+            }
+        } while (campo[y][x].equals("*"));
         winningrules(campo);
         System.out.println();
         print(campo);
@@ -143,6 +141,88 @@ void print(String[][] campo) {
 }
 
 //griglia base
+
+//cosa fare per difendersi
+void pvp() { //giocatore 1 "x", giocatpre 2 "y"
+    String[][] campo;
+    campo = new String[3][3];
+    campo = griglia(campo);
+    String annulla_mossa;
+    Scanner in = new Scanner(System.in);
+    int y, x;
+    /*    /==\
+           ||
+         setup */
+    print(campo);
+    while (true) {
+        do {
+            System.out.println("giocatore 1, inserire coordinata X");
+            x = in.nextInt();
+            System.out.println("giocatore 1, inserire coordinata Y");
+            y = in.nextInt();
+            if (campo[y][x].equals("*"))
+                campo[y][x] = "x";
+            else if (!campo[y][x].equals("*"))
+                System.out.println("sei un mona, rifai");
+            System.out.println();
+            print(campo);
+            do {
+                System.out.println("vuoi annullare la mossa (Si / No)");
+                annulla_mossa = in.nextLine();
+            } while (!annulla_mossa.equals("Si") && !annulla_mossa.equals("No"));
+            if (annulla_mossa.equals("Si"))
+                campo[y][x] = "*";
+        } while (!campo[y][x].equals("x") || annulla_mossa.equals("Si"));
+        winningrules(campo);
+
+        do {
+            System.out.println("giocatore 2, inserire coordinata X");
+            x = in.nextInt();
+            System.out.println("giocatore 2, inserire coordinata Y");
+            y = in.nextInt();
+            if (campo[y][x].equals("*"))
+                campo[y][x] = "o";
+            else if (!campo[y][x].equals("*"))
+                System.out.println("sei un mona, rifai");
+            System.out.println();
+            print(campo);
+            do {
+                System.out.println("vuoi annullare la mossa (Si / No)");
+                annulla_mossa = in.nextLine();
+            } while (!annulla_mossa.equals("Si") && !annulla_mossa.equals("No"));
+            if (annulla_mossa.equals("Si"))
+                campo[y][x] = "*";
+        } while (!campo[y][x].equals("x") || annulla_mossa.equals("Si"));
+        winningrules(campo);
+    }
+}
+
+//suono vittoria PvP giocatore 1 e PvE
+void playSoundwin() {
+    try {
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sounds/win.wa").getAbsoluteFile());
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+    } catch (Exception ex) {
+        System.out.println("Error with playing sound.");
+        ex.printStackTrace();
+    }
+}
+
+//suuono sconfitta PvE e vittoria PvP giocatore 2
+void playSoundlose() {
+    try {
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sounds/lose.wav").getAbsoluteFile());
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+    } catch (Exception ex) {
+        System.out.println("Error with playing sound.");
+        ex.printStackTrace();
+    }
+}
+
 String[][] griglia(String[][] griglia) {
     griglia[0][0] = "*";
     griglia[0][1] = "*";
@@ -156,8 +236,59 @@ String[][] griglia(String[][] griglia) {
     return griglia;
 }
 
+void winningrules(String[][] campo) {
+    //giocatore 1
+    if (campo[0][0].equals("x") && campo[1][0].equals("x") && campo[2][0].equals("x")) { //prima colonna
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[0][1].equals("x") && campo[1][1].equals("x") && campo[2][1].equals("*x")) { //seconda colonna
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[0][2].equals("x") && campo[1][2].equals("x") && campo[2][2].equals("x")) { //terza colonna
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[0][0].equals("x") && campo[0][1].equals("x") && campo[0][2].equals("x")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[1][0].equals("x") && campo[1][1].equals("x") && campo[1][2].equals("x")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[2][0].equals("x") && campo[2][1].equals("x") && campo[2][2].equals("x")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[0][0].equals("x") && campo[1][1].equals("x") && campo[2][2].equals("x")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[2][0].equals("x") && campo[1][1].equals("x") && campo[0][2].equals("x")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundwin();
+    } else if (campo[0][0].equals("o") && campo[1][0].equals("o") && campo[2][0].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    } else if (campo[0][1].equals("o") && campo[1][1].equals("o") && campo[2][1].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    } else if (campo[0][2].equals("o") && campo[1][2].equals("o") && campo[2][2].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    } else if (campo[0][0].equals("o") && campo[0][1].equals("o") && campo[0][2].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    } else if (campo[1][0].equals("o") && campo[1][1].equals("o") && campo[1][2].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    } else if (campo[2][0].equals("o") && campo[2][1].equals("o") && campo[2][2].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    } else if (campo[0][0].equals("o") && campo[1][1].equals("o") && campo[2][2].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    } else if (campo[2][0].equals("o") && campo[1][1].equals("o") && campo[0][2].equals("o")) { //prima riga
+        System.out.println("Abbiamo un vincitore!!!");
+        playSoundlose();
+    }
+}
 
-//cosa fare per difendersi
 String[][] computer(String[][] campo) throws InterruptedException {
     if ((campo[0][0].equals("x") && campo[1][0].equals("x")) && campo[2][0].equals("*")) //prima colonna
         campo[2][0] = "o";
@@ -218,137 +349,4 @@ String[][] computer(String[][] campo) throws InterruptedException {
         } while (campo[y][x].equals("o"));
     }
     return campo;
-}
-
-void pvp() { //giocatore 1 "x", giocatpre 2 "y"
-    String[][] campo;
-    campo = new String[3][3];
-    campo = griglia(campo);
-    String annulla_mossa;
-    Scanner in = new Scanner(System.in);
-    int y, x;
-    /*    /==\
-           ||
-         setup */
-    print(campo);
-    while (true) {
-        do {
-            System.out.println("giocatore 1, inserire coordinata X");
-            x = in.nextInt();
-            System.out.println("giocatore 1, inserire coordinata Y");
-            y = in.nextInt();
-            if (campo[y][x].equals("*"))
-                campo[y][x] = "x";
-            else if (!campo[y][x].equals("*"))
-                System.out.println("sei un mona, rifai");
-            System.out.println();
-            print(campo);
-            do {
-                System.out.println("vuoi annullare la mossa (Si / No)");
-                annulla_mossa = in.nextLine();
-            } while (!annulla_mossa.equals("Si") && !annulla_mossa.equals("No"));
-            if (annulla_mossa.equals("Si"))
-                campo[y][x] = "*";
-        } while (!campo[y][x].equals("x") || annulla_mossa.equals("Si"));
-        winningrules(campo);
-        
-        do {
-            System.out.println("giocatore 2, inserire coordinata X");
-            x = in.nextInt();
-            System.out.println("giocatore 2, inserire coordinata Y");
-            y = in.nextInt();
-            if (campo[y][x].equals("*"))
-                campo[y][x] = "o";
-            else if (!campo[y][x].equals("*"))
-                System.out.println("sei un mona, rifai");
-            System.out.println();
-            print(campo);
-            do {
-                System.out.println("vuoi annullare la mossa (Si / No)");
-                annulla_mossa = in.nextLine();
-            } while (!annulla_mossa.equals("Si") && !annulla_mossa.equals("No"));
-            if (annulla_mossa.equals("Si"))
-                campo[y][x] = "*";
-        } while (!campo[y][x].equals("x") || annulla_mossa.equals("Si"));
-        winningrules(campo);
-    }
-}
-
-void winningrules(String[][] campo) {
-    //giocatore 1
-    if (campo[0][0].equals("x") && campo[1][0].equals("x") && campo[2][0].equals("x")) { //prima colonna
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[0][1].equals("x") && campo[1][1].equals("x") && campo[2][1].equals("*x")) { //seconda colonna
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[0][2].equals("x") && campo[1][2].equals("x") && campo[2][2].equals("x")) { //terza colonna
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[0][0].equals("x") && campo[0][1].equals("x") && campo[0][2].equals("x")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[1][0].equals("x") && campo[1][1].equals("x") && campo[1][2].equals("x")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[2][0].equals("x") && campo[2][1].equals("x") && campo[2][2].equals("x")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[0][0].equals("x") && campo[1][1].equals("x") && campo[2][2].equals("x")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[2][0].equals("x") && campo[1][1].equals("x") && campo[0][2].equals("x")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundwin();
-    } else if (campo[0][0].equals("o") && campo[1][0].equals("o") && campo[2][0].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    } else if (campo[0][1].equals("o") && campo[1][1].equals("o") && campo[2][1].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    } else if (campo[0][2].equals("o") && campo[1][2].equals("o") && campo[2][2].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    } else if (campo[0][0].equals("o") && campo[0][1].equals("o") && campo[0][2].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    } else if (campo[1][0].equals("o") && campo[1][1].equals("o") && campo[1][2].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    } else if (campo[2][0].equals("o") && campo[2][1].equals("o") && campo[2][2].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    } else if (campo[0][0].equals("o") && campo[1][1].equals("o") && campo[2][2].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    } else if (campo[2][0].equals("o") && campo[1][1].equals("o") && campo[0][2].equals("o")) { //prima riga
-        System.out.println("Abbiamo un vincitore!!!");
-        playSoundlose();
-    }
-}
-
-//suono vittoria PvP giocatore 1 e PvE
-void playSoundwin() {
-    try {
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sounds/win.wa").getAbsoluteFile());
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioInputStream);
-        clip.start();
-    } catch (Exception ex) {
-        System.out.println("Error with playing sound.");
-        ex.printStackTrace();
-    }
-}
-
-//suuono sconfitta PvE e vittoria PvP giocatore 2
-void playSoundlose() {
-    try {
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("sounds/lose.wav").getAbsoluteFile());
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioInputStream);
-        clip.start();
-    } catch (Exception ex) {
-        System.out.println("Error with playing sound.");
-        ex.printStackTrace();
-    }
 }
